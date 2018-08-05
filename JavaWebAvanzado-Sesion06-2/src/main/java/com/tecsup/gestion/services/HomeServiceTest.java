bec18e74c79f0fa627626e80d70d061786b1c207
Alumno <Alumno@LAB701-11>
2018-08-05 13:42:32 -0500

    aaaaaaa

---
+++ JavaWebAvanzado-Sesion06-2/src/main/java/com/tecsup/gestion/services/HomeServiceTest.java
@@ -0,0 +1,58 @@
+package com.tecsup.gestion.services;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.tecsup.gestion.model.User;
+
+import junit.framework.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextHierarchy({
+	  @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml"),
+	  @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
+	})
+@WebAppConfiguration
+public class HomeServiceTest {
+
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeServiceTest.class);
+
+	
+	@Autowired
+	private HomeService homeService;
+
+	@Test
+	public void testGetUserInfo() {
+
+		try {
+			//
+			
+			int I_USER_ID = 100 ;
+			String O_ADDRESS = "Av. Lima";
+			
+			User usr = homeService.findUserById(I_USER_ID);	
+
+			logger.info(usr.toString());
+			
+			Assert.assertEquals(O_ADDRESS, usr.getAddress());
+			
+			
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+
+	}
+
+
+	
+}
